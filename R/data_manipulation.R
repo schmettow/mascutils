c4c011fce3f24979d1232746cdabeae74de55198
Martin Schmettow <schmettow@web.de>
2018-04-05 19:28:42 +0200

    + switching to tidy eval with rlang + gofirst/go_arrange make use of multiple columns + gofirst/go_arrange no longer take formulas as parameters + z_score works with ranges of columns + z_score now always does na.rm = T and adds columns + as.tbl_obs always creates a variable Obs + split the print methods for as_tbl_obs (normal and knitr) + NEWS file added

--- R/data_manipulation.R
+++ R/data_manipulation.R
@@ -40,8 +40,8 @@ expand_grid <-
 
 go_first <-
   function(D, ...){
-    args <- list(...)
-    df1 <- dplyr::select_(D, .dots = args)
+    cols <- quos(...)
+    df1 <- dplyr::select(D, !!!cols)
     mascutils::left_union(df1, D)
   }
 
@@ -51,10 +51,10 @@ go_first <-
 
 go_arrange <-
   function(D, ...){
-    args <- list(...)
-    df1 <- select_(D, .dots = args)
-    D <- mascutils::left_union(df1, D)
-    dplyr::arrange_(D, .dots = args)
+    cols <- quos(...)
+    df1 <- dplyr::select(D, !!!cols)
+    mascutils::left_union(df1, D) %>%
+      arrange(!!!cols)
   }
 
 
@@ -89,9 +89,7 @@ left_union <-
 #' The (simple) range of columns are z-transformed and added to the data frame (foo_z)
 #'
 #' @param D data frame
-#' @param range expression
-#' @param na.rm ignoring missing values (TRUE)
-#' @param add adding z scores (TRUE)
+#' @param ... dplyr::select range of variables
 #' @return data frame
 #'
 #'
@@ -100,29 +98,16 @@ left_union <-
 #' @import tidyr
 #' @export
 
-## TODO: change to f_eval style and formulas
 
-z_score <-
-  function(D, range, na.rm = T, add = T){
-    range <- substitute(range) ## catching column specification, changing context to local
-    out <-
-      D %>%
-      select(eval(range)) %>%
-      mutate(ID = row_number()) %>%
-      gather(var, val, na.rm = F, -ID) %>%
-      mutate(z_var = stringr::str_c("z", var, sep = "")) %>%
-      group_by(var) %>%
-      mutate(m = mean(val, na.rm = na.rm),
-             s = sd(val, na.rm = na.rm),
-             z_val = (val - m)/s) %>%
-      ungroup() %>%
-      select(ID, z_var, z_val) %>%
-      spread(z_var, z_val) %>%
-      select(-ID)
-    if(add) out <- bind_cols(D, out)
-    class(out) <- class(D)
-    out
-  }
+z_score <- function(D,  ...){
+  col_spec <- quos(...)
+  df_z <- dplyr::select(D, !!!col_spec) %>%
+    transmute_all(z)
+  names(df_z) <- stringr::str_c("z", names(df_z), sep = "")
+  bind_cols(D, df_z)
+}
+
+z <- function(x) (x - mean(x, na.rm = T))/sd(x, na.rm = T)
 
 ## TODO:
 # check out dplyr:::select_.data.frame for how to use dplyrs column expansion
